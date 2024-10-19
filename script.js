e13026859198f66f8c87db4bf55626e49a4098c4
Nikhil-197 <mahajannikhil177@gmail.com>
2024-10-19 07:35:25 +0530

    First

---
+++ script.js
@@ -0,0 +1,5 @@
+const moreBTN = document.querySelector(".more");
+moreBTN.addEventListener("mouseover", () => {
+  const tags = document.querySelector(".tags");
+  tags.style.display = "block";
+});
